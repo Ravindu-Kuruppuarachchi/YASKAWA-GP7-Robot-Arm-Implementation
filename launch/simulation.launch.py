8b28aa3a53cb634eb6958d91d5befcb47591b5a1
Ravindu Kuruppuarachchi <ravindukrashmika@gmail.com>
2025-11-29 12:04:43 +0530

    gazebo simulation

---
+++ launch/simulation.launch.py
@@ -0,0 +1,62 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription, SetEnvironmentVariable
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    # 1. Get Paths
+    pkg_gp7_sim = get_package_share_directory('gp7_sim')
+    ros_gz_sim_pkg = get_package_share_directory('ros_gz_sim')
+
+    # 2. Set the Mesh Path (Environment Variable)
+    # This fixes the "Unable to find file" error automatically
+    set_env_vars = SetEnvironmentVariable(
+        name='GZ_SIM_RESOURCE_PATH',
+        value=[os.path.join(pkg_gp7_sim, '..', '..', 'src'), ':' + os.environ.get('HOME') + '/ros2_ws/src']
+    )
+
+    # 3. Launch Gazebo with the World (Boxes)
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(ros_gz_sim_pkg, 'launch', 'gz_sim.launch.py')
+        ),
+        launch_arguments={'gz_args': f'-r {os.path.join(pkg_gp7_sim, "worlds", "project.sdf")}'}.items(),
+    )
+
+    # 4. Spawn the Robot
+    spawn_robot = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=[
+            '-file', os.path.join(pkg_gp7_sim, 'urdf', 'gp7.urdf'),
+            '-name', 'gp7_robot',
+            '-x', '0', '-y', '0', '-z', '0'
+        ],
+        output='screen'
+    )
+
+    # 5. Start the Bridge (MATLAB Connection)
+    # Bridges all 6 joints + clock
+    bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=[
+            '/model/gp7_robot/joint/joint_1_s/cmd_pos@std_msgs/msg/Float64]ignition.msgs.Double',
+            '/model/gp7_robot/joint/joint_2_l/cmd_pos@std_msgs/msg/Float64]ignition.msgs.Double',
+            '/model/gp7_robot/joint/joint_3_u/cmd_pos@std_msgs/msg/Float64]ignition.msgs.Double',
+            '/model/gp7_robot/joint/joint_4_r/cmd_pos@std_msgs/msg/Float64]ignition.msgs.Double',
+            '/model/gp7_robot/joint/joint_5_b/cmd_pos@std_msgs/msg/Float64]ignition.msgs.Double',
+            '/model/gp7_robot/joint/joint_6_t/cmd_pos@std_msgs/msg/Float64]ignition.msgs.Double',
+            '/clock@rosgraph_msgs/msg/Clock[ignition.msgs.Clock'
+        ],
+        output='screen'
+    )
+
+    return LaunchDescription([
+        set_env_vars,
+        gazebo,
+        spawn_robot,
+        bridge
+    ])
---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = 'gp7_sim'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+    (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
+    (os.path.join('share', package_name, 'meshes/gp7/visual'), glob('meshes/gp7/visual/*')),
+    (os.path.join('share', package_name, 'meshes/gp7/collision'), glob('meshes/gp7/collision/*')),
+    (os.path.join('share', package_name, 'worlds'), glob('worlds/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ravindu-rashmika',
+    maintainer_email='ravindukrashmika@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
